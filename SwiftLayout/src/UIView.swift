e34e08b3b8ebaca19d32572648ad8325963788d0
fjbelchi <fjbelchi@gmail.com>
2015-07-01 13:30:24 +0200

    Installing constraints from ios 8+

--- SwiftLayout/src/UIView.swift
+++ SwiftLayout/src/UIView.swift
@@ -20,76 +20,16 @@ public extension UIView {
             return []
         }
         set {
-            objc_setAssociatedObject(self, &layoutConstraintsKey, newValue, UInt(OBJC_ASSOCIATION_RETAIN_NONATOMIC))
+            objc_setAssociatedObject(self, &layoutConstraintsKey, newValue, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
         }
-    }
-    
-    public func sl_setWidth(width: Float) -> NSLayoutConstraint {
-        return NSLayoutConstraint(item: self, attribute: .Width, relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute, multiplier: 1.0, constant: CGFloat(width))
-    }
-    
-    public func sl_setHeight(width: Float) -> NSLayoutConstraint {
-        return NSLayoutConstraint(item: self, attribute: .Height, relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute, multiplier: 1.0, constant: CGFloat(width))
-    }
-    
-    public func sl_stackHorizontal(view: UIView) -> NSLayoutConstraint {
-        return NSLayoutConstraint(item: self, attribute: .Leading, relatedBy: .Equal, toItem: view, attribute: .Trailing, multiplier: 1.0, constant: 0.0)
-    }
-    
-    public func sl_stackVertical(view: UIView) -> NSLayoutConstraint {
-        return NSLayoutConstraint(item: self, attribute: .Top, relatedBy: .Equal, toItem: view, attribute: .Bottom, multiplier: 1.0, constant: 0.0)
-    }
-    
-    public func sl_superviews() -> Array<UIView> {
-        var superviews : Array<UIView> = []
-        var view : UIView? = self.superview
-        while view != nil {
-            superviews.append(view!)
-            if let superview = view!.superview {
-                view = superview
-            } else {
-                view = nil
-            }
-        }
-        
-        return superviews
-    }
-    
-    public func sl_isAncestorOf(view: UIView) -> Bool {
-        return contains(view.sl_superviews(), self) // update for swift 2
-    }
-    
-    public func sl_nearestCommonViewAncestor(view: UIView) -> UIView? {
-        if (self == view) {
-            return self
-        }
-        
-        if (self.sl_isAncestorOf(view)) {
-            return self
-        }
-        
-        if (view.sl_isAncestorOf(self)) {
-            return view
-        }
-        
-        let ancestors = self.sl_superviews()
-        
-        for viewInSuperviews in view.sl_superviews() {
-            if contains(ancestors,viewInSuperviews) { // update for swift 2
-                return viewInSuperviews
-            }
-        }
-        
-        return nil;
     }
     
     public func sl_installConstrains() {
-        var constrains = Array<NSLayoutConstraint>()
+        var constraints = Array<NSLayoutConstraint>()
         for view in self.subviews {
-            view.setTranslatesAutoresizingMaskIntoConstraints(false)
-            for constraint in view.autolayoutConstraints {
-                constraint.sl_install()
-            }
+            constraints += view.autolayoutConstraints;
         }
+        
+        NSLayoutConstraint.activateConstraints(constraints)
     }
 }
